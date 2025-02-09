101f9ee8b5e3ff993886fe7c9db5f7213d21dffa
OrlovAlexandr <eaglophone@gmail.com>
2025-02-09 11:44:05 +0500

    Refactor movies.py: optimized get_original_title function, items.py: necessary fields added

--- movies_parser/movies_parser/items.py
+++ movies_parser/movies_parser/items.py
@@ -9,7 +9,9 @@ import scrapy
 class MoviesParserItem(scrapy.Item):
     # define the fields for your item here like:
     title = scrapy.Field()
+    original_title = scrapy.Field()
     genre = scrapy.Field()
     director = scrapy.Field()
     country = scrapy.Field()
     year = scrapy.Field()
+    imdb_rating = scrapy.Field()
